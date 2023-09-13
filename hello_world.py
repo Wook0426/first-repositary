0905810ce691cf3c348fcd4332a42a9b3b11d42b
Wook0426 <t01053635562@gmail.com>
2023-09-13 15:21:12 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -3,4 +3,13 @@ def print_hello():
 
 if __name__ == '__main__':
     print_hello()
-    
+
+def print_hello():
+    print("Hello World!!")
+
+def print_KIT():
+    print("Kumoh National Institute of Technology.")
+
+if __name__ == '__main__’:
+    print_KIT()
+    print_hello()
